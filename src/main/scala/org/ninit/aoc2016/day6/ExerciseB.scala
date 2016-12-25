b3fb2a6ed88122ab65ef033124290414acf3b5d0
Joaquin Perez-Iglesias <joaquin.perez.iglesias@gmail.com>
2016-12-25 09:22:15 +0100

    Add day6-B

---
+++ src/main/scala/org/ninit/aoc2016/day6/ExerciseB.scala
@@ -0,0 +1,13 @@
+package org.ninit.aoc2016.day6
+
+object ExerciseB extends App {
+
+  private def count(input: String, size: Int): String = {
+    val data = read(input)
+    List.range(0, size - 1).map { index =>
+      data.map(_.charAt(index)).groupBy(identity).map(e => e._1 -> e._2.length).toVector.sortWith(_._2 > _._2).last._1
+    }.mkString
+  }
+
+  println(count("/day6", 9))
+}
